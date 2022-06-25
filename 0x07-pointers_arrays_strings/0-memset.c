b56f4d7b570d34a27548f47e54cfa95d120b710f
Barkitad <barktadesse@gmail.com>
2022-06-25 07:55:38 -0700

    dfrt

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,26 +3,14 @@
  * _memset - sets memory to array
  * @s: array to set
  * @b: value to set it as
-<<<<<<< HEAD
  * @n: n amount of times
-=======
- * @n: n amountf of times
->>>>>>> 04a03cfaa6f3f1c90fb4b0c172ed5ecab0c97903
  * Return: char value of s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-<<<<<<< HEAD
 	unsigned int count;
 
 	for (count = 0; count < n; count++)
 		s[count] = b;
 	return (s);
 }
-=======
-  unsigned int count;
-
-  for (count = 0; count < n; count++)
-    s[count] = b;
-  return (s);
->>>>>>> 04a03cfaa6f3f1c90fb4b0c172ed5ecab0c97903
